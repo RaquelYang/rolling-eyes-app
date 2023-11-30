aa1e0d205e22ee523981653945732ea9e260bf16
Raquel <rauqel@pollex.com.tw>
2023-11-30 16:39:36 +0800

    add compodocrc package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
@@ -12,11 +13,28 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent implements OnInit {
     title = 'rolling-eyes-app';
 
+    constructor(private http: HttpClient) {}
+
     ngOnInit(): void {
         const a = 123;
         // const multiple = 123;
         const aApplee = 'aaa';
         console.log('a', a);
         console.log('a', aApplee);
+    }
+    // https://ithelp.ithome.com.tw/articles/10296508
+    /**
+     * 取得 api
+     * @param aaa - 假的
+     * @param {Type} [Name] [Description]
+     * @param 定義一個參數的類型和描述
+      @returns 描述返回值
+      @example 定義一個示例用法
+      @link 定義鏈接另一個方法、文檔或外部鏈接
+      @ignore 表示標記的內容永遠不會出現在文檔中
+     */
+    getAPI(aaa: string): void {
+        console.log('aaa', aaa);
+        this.http.get('aaa');
     }
 }
