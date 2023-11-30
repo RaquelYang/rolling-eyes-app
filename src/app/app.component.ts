9f1351c6f559bc177ebc8ca5cc463f67c941f60e
Raquel <rauqel@pollex.com.tw>
2023-11-30 15:57:05 +0800

    init project + eslint + prettier
    1. 初始專案
    2. 加入 eslint + prettier 套件

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+    title = 'rolling-eyes-app';
+
+    ngOnInit(): void {
+        const a = 123;
+        const Multiple = 123;
+        const aApplee = 'aaa';
+        console.log('a', a);
+        console.log('a', aApplee);
+    }
+}
